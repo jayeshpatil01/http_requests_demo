43ebf3efe7eb9bbbf6e10bcf90688be1f431d8ba
Jay <jayeshpatil8041@gmail.com>
2024-07-17 22:40:54 +0530

    get request and parse the json response

--- requests.rb
+++ requests.rb
@@ -5,3 +5,7 @@ require 'json'
 response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
 puts response.body
 
+# Try manipulating the response received from get request
+response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+parsed_response = JSON.parse(response.body)
+puts parsed_response['title']
