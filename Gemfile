40d9ecb71f1c8be1377ca44323c52e77bf2a16ae
Jay <jayeshpatil8041@gmail.com>
2024-07-17 23:11:13 +0530

    add nokogiri to handle other response format

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org/'
 
 gem 'httparty', '~> 0.22.0'
+gem 'nokogiri', '~> 1.16', '>= 1.16.6'
--- requests.rb
+++ requests.rb
@@ -1,60 +1,66 @@
 require 'httparty'
 require 'json'
+require 'nokogiri'
 
-# Try fetching simple response with get request
-response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
-puts response.body
+# # Try fetching simple response with get request
+# response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+# puts response.body
 
-# Try manipulating the response received from get request
-response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
-parsed_response = JSON.parse(response.body)
-puts parsed_response['title']
+# # Try manipulating the response received from get request
+# response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+# parsed_response = JSON.parse(response.body)
+# puts parsed_response['title']
 
-#  example post syntax
-# options = {
+# #  example post syntax
+# # options = {
+# #   body: {
+# #     key: 'value'
+# #   }
+# # }
+# # response = HTTParty.post('URL', options)
+#  options = {
 #   body: {
-#     key: 'value'
-#   }
-# }
-# response = HTTParty.post('URL', options)
- options = {
-  body: {
-    title: 'foo',
-    body: 'bar',
-    userId: 1
-  }.to_json,
-  headers: { 'Content-Type' => 'application/json' }
- }
+#     title: 'foo',
+#     body: 'bar',
+#     userId: 1
+#   }.to_json,
+#   headers: { 'Content-Type' => 'application/json' }
+#  }
 
- response = HTTParty.post('https://jsonplaceholder.typicode.com/posts', options)
- puts response.body
+#  response = HTTParty.post('https://jsonplaceholder.typicode.com/posts', options)
+#  puts response.body
 
- # checking the response status code and handle errors
- options = {
-  body: {
-    title: 'foo',
-    body: 'bar',
-    userId: 1
-  }.to_json,
-  headers: { 'Content-Type' => 'application/json' }
- }
+#  # checking the response status code and handle errors
+#  options = {
+#   body: {
+#     title: 'foo',
+#     body: 'bar',
+#     userId: 1
+#   }.to_json,
+#   headers: { 'Content-Type' => 'application/json' }
+#  }
 
- response = HTTParty.post('https://jsonplaceholder.typicode.com/posts', options)
+#  response = HTTParty.post('https://jsonplaceholder.typicode.com/posts', options)
 
- if response.code == 201
-  puts "Post created successfull"
-  puts response.body
- else
-  puts "Failed to create a post. Error: #{response.message}"
- end
+#  if response.code == 201
+#   puts "Post created successfull"
+#   puts response.body
+#  else
+#   puts "Failed to create a post. Error: #{response.message}"
+#  end
 
 
- ### Accessing multiple endpoints
-urls = ["https://jsonplaceholder.typicode.com/posts/1", "https://jsonplaceholder.typicode.com/posts/2"]
+#  ### Accessing multiple endpoints
+# urls = ["https://jsonplaceholder.typicode.com/posts/1", "https://jsonplaceholder.typicode.com/posts/2"]
 
-responses = urls.map { |url| HTTParty.get(url) }
+# responses = urls.map { |url| HTTParty.get(url) }
 
-responses.each do |response|
-  parsed_response = JSON.parse(response.body)
-  puts parsed_response['title']
-end
+# responses.each do |response|
+#   parsed_response = JSON.parse(response.body)
+#   puts parsed_response['title']
+# end
+
+response = HTTParty.get('https://www.w3schools.com/xml/node.xml')
+xml_doc = Nokogiri::XML(response.body)
+puts xml_doc
+puts xml_doc.xpath('//note//to').text
