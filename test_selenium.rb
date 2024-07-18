b1776d565a2fa5fac60557982e09c80b7ecb90a3
Jay <jayeshpatil8041@gmail.com>
2024-07-18 14:55:50 +0530

    add awesomeprint and fetch item description by crawling the item page

--- test_selenium.rb
+++ test_selenium.rb
@@ -1,5 +1,6 @@
 require 'selenium-webdriver'
 require 'nokogiri'
+require "awesome_print"
 require 'pry'
 
 options = Selenium::WebDriver::Chrome::Options.new
@@ -11,20 +12,28 @@ url = 'https://scrapingclub.com/exercise/list_infinite_scroll/'
 
 driver.get(url)
 
-def extract_items(page_source)
+def extract_items(page_source, url, driver)
   html = Nokogiri::HTML(page_source)
-  items = {}
+  items = []
   html.css('.w-full.rounded.border.post').count
   html.css('.w-full.rounded.border.post').each do |item|
-    items[items.count + 1] = [ item.at_css('h4 a').text,  item.at_css('h5').text ]
+    parsed_url = URI.parse(url)
+    item_link = "#{parsed_url.scheme}://#{parsed_url.host}#{item.at_css('h4 a')['href']}"
+    items << { title: item.at_css('h4 a').text,  price: item.at_css('h5').text, link: item_link }
+  end
+  item_details = []
+  items.map! do |item|
+    driver.get(item[:link])
+    html = Nokogiri::HTML(driver.page_source)
+    item['decription'] = html.at_css('p.card-description').text
   end
   items
 end
 
 all_items = []
 previous_height = 0
-5.downto(1) do
-  all_items << extract_items(driver.page_source)
+1.downto(1) do
+  all_items << extract_items(driver.page_source, url, driver)
 
   driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 
@@ -34,4 +43,4 @@ previous_height = 0
   previous_height = new_height
 end
 
-puts extract_items(driver.page_source)
+puts ap all_items
